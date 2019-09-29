d80eca994e59be3256eaa82cc3a31f84c4df8b40
Harihar98 <30701256+Harihar98@users.noreply.github.com>
2019-09-29 19:47:29 +0530

    Add files via upload

---
+++ gcd_euclid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a,int b){
+	int c;
+	while(b>0){
+		c=a%b;
+		a=b;
+		b=c;
+	}
+	return a;
+}
+
+int main(){
+	int a,b;
+	cout<<"Enter 2 nos. : ";
+	cin>>a>>b;
+	cout<<"\nGCD("<<a<<","<<b<<") = "<<gcd(a,b)<<endl;
+	return 0;
+}
